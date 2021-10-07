ba57696ac7ec9814a68f0876da8b3bca19269a34
Ethan <wlx@buaa.edu.cn>
2021-10-07 18:29:07 +0800

    implement image manipulating methods

--- utils.py
+++ utils.py
@@ -1,17 +1,32 @@
 import os
 import numpy as np
 
-def read_image(*args, **kwargs):
-    # TODO: 
-    pass
+from PIL import Image
 
-def read_images(path, dtype, shape=None):
-    """[summary]
+def read_image(path, shape=None, resample=None):
+    """ read single image
+
+    Args:
+        path ([type]): [description]
+        shape ([type], optional): [description]. Defaults to None.
+        resample ([type], optional): [description]. Defaults to None.
+
+    Returns:
+        [type]: [description]
+    """
+    with Image.open(path) as img:
+        if shape is not None:
+            img = img.resize(shape, resample)
+    return np.asarray(img)
+
+def read_images(path, dtype, shape=None, resample=None):
+    """ read images from directory path
 
     Args:
         path ([type]): [description]
         dtype ([type]): [description]
         shape ([type], optional): [description]. Defaults to None.
+        resample ([type], optional): [description]. Defaults to None.
 
     Returns:
         [type]: [description]
@@ -20,32 +35,12 @@ def read_images(path, dtype, shape=None):
     valid_suffix = \
         set([".jpg", ".gif", ".png", 
             ".tga", ".jpeg", ".ppm"])
-    for img in sorted(os.listdir(path)):
-        ext = os.path.splitext(img)[-1]
+    for img_path in sorted(os.listdir(path)):
+        ext = os.path.splitext(img_path)[-1]
         if ext.lower() not in valid_suffix:
             continue
-        # TODO: 
-        img = read_image()
-
-        if shape is not None:
-            # TODO: reshape img
-            pass    
-
+        img = read_image(img_path, shape, resample)
         imgs.append(img)
-    
+    imgs = np.stack(imgs, axis=0)
+
     return imgs
-
-
-def resize_image(arr, shape):
-    """[summary]
-
-    Args:
-        arr ([type]): [description]
-        shape ([type]): [description]
-
-    Returns:
-        [type]: [description]
-    """
-    
-    return np.zeros_like(arr)
-
