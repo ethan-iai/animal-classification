1452b97ba48c78a18a61fb9fea14e727ae438415
Ethan <wlx@buaa.edu.cn>
2021-10-09 20:59:18 +0800

    update inference script

--- train.sh
+++ train.sh
@@ -2,6 +2,10 @@
 
 python main.py --seed 5 --name animals --expand-labels --data-path data --dataset animals --num-classes 22 --total-steps 300000 --eval-step 1000 --randaug 2 16 --batch-size 128 --teacher_lr 0.05 --student_lr 0.05 --weight-decay 5e-4 --ema 0.995 --nesterov --mu 7 --label-smoothing 0.15 --temperature 0.7 --threshold 0.6 --lambda-u 8 --warmup-steps 5000 --uda-steps 5000 --student-wait-steps 3000 --teacher-dropout 0.2 --student-dropout 0.2 --amp
 
-python imagenet data -b 1 -j 0 -a resnet50 --gpu0
+python imagenet.py data -b 1 -j 0 -a resnet50 --gpu 0
 
 & D:/anaconda/envs/torch38/python.exe d:/animal-classification/imagenet.py data -b 4 -j 0 -a resnet50 --gpu 0
+
+python infernce.py -a resnet50 -gpu 0 -b 16 -j 0 --resume model_best.pth.tar
+
+& D:/anaconda/envs/torch38/python.exe d:/animal-classification/inference.py --data data/test -a resnet50 --gpu 0 -b 16 -j 0 --resume model_best.pth.tar
